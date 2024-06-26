f5b934675de592699c3418532a4001018264ba48
amanv077 <amanverma4816@gmail.com>
2024-06-26 17:11:24 +0530

    final

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { AnnoyedIcon } from "lucide-react";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import SignOutButton from "./SignOutButton";
@@ -10,9 +9,9 @@ const Navbar = async () => {
   return (
     <header className=" py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
-        <Link href="/">
-          <AnnoyedIcon />
-        </Link>
+        <h1 className=" text-4xl font-semibold text-sky-700  hover:text-cyan-950 select-none">
+          ITax <span className="text-xl font-semibold">Easy</span>
+        </h1>
         {session?.user ? (
           <SignOutButton />
         ) : (
